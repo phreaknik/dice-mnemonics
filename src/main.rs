bd49b793e08a1e2e8f786ff355d543fece371203
John Boyd IV <johnboydiv@gmail.com>
2018-09-17 23:16:17 -0500

    [WIP] attempting to recunstruct full dictionary space with radix-6 composition of dice rolls

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::{self, BufRead, BufReader, Write};
 use std::process;
 use std::vec::Vec;
 
-const DICE_SIDES: usize = 6;
+const DICE_SIDES: u32 = 6;
 
 fn main() {
     let arg_matches = App::new(crate_name!())
@@ -36,29 +36,44 @@ fn main() {
         .lines()
         .map(|l| l.expect("Could not parse line"))
         .collect();
-    let dict_size = dictionary.len() - 1;
+    let dict_size: u32 = dictionary.len() as u32;
 
-    // Calculate minimum # of rolls to preserve the most entropy
-    let mut rem = dict_size;
-    let mut min_rolls = 0;
-    loop {
-        rem = rem / DICE_SIDES;
-
-        if rem <= 0 {
-            break;
-        }
-
-        min_rolls = min_rolls + 1;
+    // Calculate optimal dice combination to maximize entropy.
+    // This is achieved by decomposing the dictionary size into
+    // factors of base-<DICE_SIDES> numbers.
+    let mut base_digits = 0;
+    let mut max_num = 1;
+    while max_num < dict_size {
+        base_digits = base_digits + 1;
+        max_num = max_num * DICE_SIDES;
     }
 
-    // Get dice rolls from user and return dictionary word
-    println!("Enter 'q' or 'quit' to exit");
+    // Calculate radix decomposition of dictionary size
+    let mut radix_decomp: Vec<u32> = Vec::new();
+    let mut quo = dict_size;
+    let mut rem = dict_size;
+    for i in (1..base_digits).rev() {
+        quo = rem / DICE_SIDES.pow(i); // Calculate running quotient
+        rem = rem - quo*DICE_SIDES.pow(i); // Calculate remainder
+        radix_decomp.push(quo); // Add new digit to radix decomposition
+    }
+
+    // Calculate optimal number of dice to roll
+    let mut optimal_num_dice = 0;
+    let mut count = 0;
+    for n in radix_decomp.iter().rev() {
+        count = count + 1;
+        optimal_num_dice += n * count;
+    }
+
+    // Print optimal dice strategy
+    println!{"Roll {} {}-sided dice to preserve 100% of dictionary entropy.", optimal_num_dice, DICE_SIDES};
+
+   // Print basic usage instructions
     println!("Enter dice rolls (without spaces), to generate a seed words.");
-    println!(
-        "Use at least {} rolls to preserve {}% entropy.",
-        min_rolls,
-        100 * DICE_SIDES.pow(min_rolls) / dict_size
-    );
+    println!("Enter 'q' or 'quit' to exit");
+
+     // Get dice rolls from user and return dictionary word
     loop {
         let input = prompt_user("> ")
             .unwrap_or_else(|err| {
@@ -73,51 +88,54 @@ fn main() {
         }
 
         // Make sure we have enough rolls to preserve maximum entropy
-        let rolls: Vec<char> = input.chars().collect();
-        if (rolls.len() as u32) < min_rolls {
+        let rolls: Vec<u8> = input.chars()
+                                    .map(|c| {
+                                        c.to_string()
+                                            .parse()
+                                            .expect("Not a number")
+                                    }).collect();
+        if (rolls.len() as u32) < optimal_num_dice {
             println!(
                 "error: roll at least {} dice to preserve entropy",
-                min_rolls
+                optimal_num_dice
             );
             continue;
         }
 
-        // Loop for each roll to calculate large number
-        let mut num = 0;
-        let mut count = 0;
-        for x in rolls {
-            count = count + 1;
+        // Compose large number
+        let  num = calc_from_dice(2, vec![1, 1, 1, 1], vec![0, 0, 0]);
+        // let  num = calc_from_dice(DICE_SIDES, radix_decomp.clone(), rolls);
 
-            // Calculate scale factor for this roll
-            let scale_factor = dict_size / DICE_SIDES.pow(count);
 
-            // Break if we have seen enough rolls
-            if scale_factor <= 0 {
-                break;
-            }
 
-            // Convert roll from char to number
-            let roll: usize = x.to_string().parse().expect("not a number");
-
-            // Check this is a valid roll
-            if roll > DICE_SIDES || roll < 1 {
-                println!(
-                    "Error: invalid die roll: {}. Must be a number between 1-6",
-                    roll
-                );
-                break;
-            }
-
-            // Calculate next part of number
-            num = num + (roll - 1) * scale_factor;
-        }
+        // let mut num = 0;
+        // let mut order = radix_decomp.len() as u32;
+        // println!("{:?}", radix_decomp);
+        // for i in 0..order {
+        // println!("loop 1");
+        //     let n = radix_decomp[i as usize];
+        //     for _ in 0..n {
+        //         println!("loop 2");
+        //         for k in (0..order).rev() {
+        //             println!("loop 3"); 
+        //             let r = rolls.pop().unwrap();
+        //             let roll: u32 = r.to_string().parse().expect("not a number");
+        //             num = num + ((roll - 1) * DICE_SIDES.pow(k));
+        //             println!("num = {}, roll = {}, order = {}, 6^k = {}",
+        //                 num, roll, order, DICE_SIDES.pow(k)
+        //             )
+        //         }
+        //     }
+        //     order = order - 1;
+        // }
 
         // Print dictionary word corresponding to the large number
         if num > dict_size {
             println!("error: dictionary overflow");
             process::exit(1);
         }
-        println!("{}", dictionary[num]);
+        println!("{}", num);
+        println!("{}", dictionary[num as usize]);
     }
 }
 
@@ -146,4 +164,44 @@ fn prompt_user(msg: &str) -> (Result<String, String>) {
     } else {
         return Ok(s);
     }
+}
+
+fn calc_from_dice(radix: u32, pattern: Vec<u32>, rolls: Vec<u8>) -> u32 {
+    let mut order = 0;
+    let mut roll = 0;
+    let mut num = 0;
+
+    let mut first = true;
+    print!("(");
+    for n in pattern {
+        if !first {
+            print!(" + ");
+        }
+
+        for k in (0..order).rev() {
+            if !first {
+                print!(" + ");
+            }
+
+            let mut coeff = 0;
+            for _ in 0..n {
+                coeff += rolls[roll] as u32;
+                roll += 1;
+                if first {
+                    print!("{}", rolls[roll]);
+                } else {
+                    print!(" + {}", rolls[roll]);
+                }
+                first = false;
+            }
+
+            num += coeff * radix.pow(k);
+
+            print!(")*({}^{})", radix, k);
+        }
+        order += 1;
+        print!(")");
+    }
+    print!(")");
+    num
 }
